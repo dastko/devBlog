c7298a50b784526be139ac1d0d63feb3cfdf0aed
davorst <davor.stankovic@devlogic.eu>
2015-08-13 16:54:24 +0200

    Added Post and Comment controllers to backend

--- backend/api/controllers/PostController.js
+++ backend/api/controllers/PostController.js
@@ -6,6 +6,23 @@
  */
 
 module.exports = {
-	
-};
 
+    post: function (req, res) {
+        Post.create({
+            title: req.param('title'),
+            content: req.param('content'),
+            subtitle: req.param('subtitle'),
+            picture: req.param('picture'),
+            useremail: req.param('useremail'),
+        }, function postCreated(err, newPost) {
+            if (err) {
+                console.log('Error: ' + err);
+                return res.negotiate(err);
+            }
+            return res.json({
+                id: newPost.id
+            });
+        })
+    }
+
+};
