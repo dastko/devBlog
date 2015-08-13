d60add6cf20a17714c30f95bc9d92a6a59fdb020
davorst <davor.stankovic@devlogic.eu>
2015-08-13 17:01:04 +0200

    Added Signup controller to frontend

---
+++ frontend/app/signupCtrl.js
@@ -0,0 +1,28 @@
+angular.module('app').controller('SignupCtrl', ['$scope', '$http', function ($scope, $http) {
+    console.log('Signup Controller initialized...');
+
+    $scope.signupForm = {
+            loading: false
+        },
+
+        $scope.submitSignupForm = function () {
+            $scope.signupForm.loading = true;
+
+            $http.post('/signup', {
+                    name: $scope.signupForm.name,
+                    email: $scope.signupForm.email,
+                    password: $scope.signupForm.password
+
+                })
+                .then(function onSuccess(response) {
+                    console.log(response);
+                    window.location = '#/addpost';
+                })
+                .catch(function onError(err) {
+                    if (err.status == 409) {}
+                    if (err.status = 400 || 403 || 404) {}
+                }).finally(function eitherWay() {
+                    $scope.signupForm.loading = false
+                })
+        }
+}]);
