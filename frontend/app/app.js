72f228259207b841351c1ca3c194dc441639b8e7
davorst <davor.stankovic@devlogic.eu>
2015-08-13 13:41:14 +0200

    Added ui-router and some views

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,1 +1,20 @@
-angular.module('app', ['ui.router']);
+var app = angular.module('app', ['ui.router']);
+
+app.config(
+    function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/posts');
+
+        $stateProvider.
+        state('login', {
+            url: '/login',
+            templateUrl: 'login.html',
+            controller: 'LoginCtrl'
+        }).
+        state('posts', {
+            url: '/posts',
+            templateUrl: 'posts.html',
+            controller: ''
+        })
+    }
+);
--- frontend/temp/loginCtrl.js
+++ frontend/temp/loginCtrl.js
@@ -1,15 +1,6 @@
-angular.module('app').controller('loginCtrl', ['$scope', '$http', function ($scope, $http) {
+angular.module('app').controller('LoginCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.loginSubmit = function () {
-        $http.post('/login', {
-                email: $scope.email,
-                password: $scope.password,
-            })
-            .then(function onSuccess() {
-                window.location = '#/addpost';
-            })
-            .catch(function onError(err) {
-
-            });
+        console.log("Nesto");
     }
 }]);
